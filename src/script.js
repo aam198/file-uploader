5b105230babe644282f0f992ef43b10e3d0e17c2
aam198 <aam198@psu.edu>
2021-04-28 15:31:49 -0400

    modal style & function updates

--- src/script.js
+++ src/script.js
@@ -4,6 +4,11 @@ const button = document.querySelector("button");
 const icon = document.querySelector("icon");
 const input = document.querySelector("input");
 
+const nextBtn = document.getElementById("nextBtn");
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close") [0];
+const backBtn = document.getElementsByClassName("backBtn") [0];
+
 let file; 
 
 button.onclick = () => {
@@ -37,7 +42,27 @@ dropArea.addEventListener("drop", (e) =>{
     showFile(); //calling function
 });
 
+nextBtn.onclick = function() {
+  modal.style.display = "block";
+ }
+ 
+ span.onclick = function() {
+   modal.style.display = "none";
+ }
 
+ backBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+//  backBtn.onclick = function() {
+//   modal.style.display = "none";
+// }
+ 
+ window.onclick = function (event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
 
 function showFile(){
     let fileType = file.type; //getting selected file type
@@ -49,12 +74,14 @@ function showFile(){
           // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
         let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
         dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+        nextBtn.disabled = false;
       }
       fileReader.readAsDataURL(file);
     }else{
       alert("This is not an Image File!");
       dropArea.classList.remove("active");
       dragText.textContent = "Drag & Drop to Upload File";
+      nextBtn.disabled = true;
     }
   }
   
