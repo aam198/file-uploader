82ba37303f6750c43eb3d77abac4646e22f015f1
aam198 <aam198@psu.edu>
2021-05-18 16:10:38 -0400

    refactor code

--- src/script.js
+++ src/script.js
@@ -11,10 +11,8 @@ const span = document.getElementsByClassName("close") [0];
 const backBtn = document.getElementById("backBtn");
 
 
-let droppedFiles = []; //Might not need or can use for submit
 
-
-// let file;   Can uncomment if you need file 
+ 
 
 //Prevent default drag behaviors
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
@@ -32,6 +30,13 @@ let droppedFiles = []; //Might not need or can use for submit
   dropArea.addEventListener(eventName, unhighlight, false)
 });
 
+button.addEventListener("click", () => {
+  input.click();
+  dragArea.classList.add("active");
+  dragText.textContent = "Select File to Upload";
+});
+
+
 // Handle dropped files
 dropArea.addEventListener('drop', handleDrop, false)
 
@@ -46,7 +51,7 @@ function highlight() {
 }
 
 function unhighlight(e) {
-  dropArea.classList.remove('active')
+  dropArea.classList.remove('active');
 }
 
 function handleDrop(e) {
@@ -55,23 +60,6 @@ function handleDrop(e) {
 
   handleFiles(files)
 }
-
-
-button.addEventListener("click", () => {
-  input.click();
-  dragArea.classList.add("active");
-  dragText.textContent = "Select File to Upload";
-});
-
-//UnComment Below
-
-//getting user select file and [0] this means if user select multiple files then we'll select only the first one
-// input.addEventListener("change", function(){
-//     file = this.files[0];
-//     dragArea.classList.add("active");
-//     // showFile();
-//     listUpload();
-// });
 
 
 //If user Drag File Over DropArea
@@ -87,14 +75,6 @@ dragArea.addEventListener("dragleave", ()=>{
     dragText.textContent = "Drag & Drop to Upload File";
   });
 
-// dragArea.addEventListener("drop", (e) =>{
-//     e.preventDefault();
-   
-//    const dt = e.dataTransfer;
-//    const files = dt.files;
-
-//    handleFiles(files);
-// });
 
 // Progress Bar when uploading files
 
@@ -123,24 +103,18 @@ function handleFiles(files) {
   initializeProgress(files.length);
   files.forEach(uploadFile);
   files.forEach(previewFile);
-  if(files >= 0){
+  if(files.length <= 0){
     nextBtn.disabled = true;
   }
   else {
     nextBtn.disabled = false;
   }
-
 }
 
 function previewFile(file) {
   let reader = new FileReader()
   reader.readAsDataURL(file)
- 
-  console.log(file);
-
-  let myItem = input.value;
-    input.value= "";
-
+  
     dragArea.classList.remove("active");
     const list = document.querySelector('ul');
     const listItem = document.createElement('li');
@@ -159,20 +133,16 @@ function previewFile(file) {
      close.className="fas fa-times";
      
       
-    close.addEventListener("click", function(){
+    close.addEventListener("click", () =>{
       setTimeout(function(){
         list.removeChild(listItem);
       }, 1000);
         listItem.classList.remove('fadeIn');
         listItem.classList.add('fadeOut');
-        if(listItem <= 0){
-          nextBtn.disabled = true;
-        }
-        else {
-          nextBtn.disabled = false;
-        }
     });
+
 }
+
 
 
 
@@ -226,54 +196,3 @@ function uploadFile(file, i) {
   xhr.send(formData);
 }
 
-//   function listUpload () {
-//     dragArea.classList.remove("active");
-//     const list = document.querySelector('ul');
-//     const input = document.querySelector('input');
-//     const files = document.querySelector('file');
-
-//     const btn = document.querySelector('button');
-
-//     let myItem = input.value;
-//     input.value= "";
-   
-//   if (myItem !== "" ){ 
-    
-    
-    
-//      //Creating 3 new elements and store them in variables 
-//      const listItem = document.createElement('li');
-//      const listText = document.createElement('span');
-//      const close = document.createElement('i');
-   
-//     listItem.classList.add('fadeIn');
-   
-//      list.appendChild(listItem);
-//      listItem.appendChild(listText);
-//      listText.textContent = myItem;
-
-
-//      listItem.appendChild(close);
-//      close.className="fas fa-times";
-     
-      
-//     close.addEventListener("click", function(e){
-//       setTimeout(function(){
-//         list.removeChild(listItem);
-//       }, 1000);
-//         listItem.classList.remove('fadeIn');
-//         listItem.classList.add('fadeOut');
-//     });
-
-     
-    
-//      input.focus();
-//   }
-  
-//   else{
-//    alert("Please select a file.");
-//    }
-//  }
-
-
-  
